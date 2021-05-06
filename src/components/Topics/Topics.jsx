f9eddbb368b056344a4d192cc815d75f6561952d
Md Shahazad Uddin <shanudjn@gmail.com>
2021-05-06 13:05:07 +0530

    added favicon

--- src/components/Topics/Topics.jsx
+++ src/components/Topics/Topics.jsx
@@ -5,7 +5,7 @@ export function Topics({ handleSetFilter }) {
         <div className="topics">
 
             {/* <button className="topics-button btn btn-primary" onClick={() => handleSetFilter("finance")}>Personal Finance</button> */}
-            <button className="topics-button btn btn-primary" onClick={() => handleSetFilter("")}>Show All Videos</button>
+            <button className="topics-button btn btn-primary" onClick={() => handleSetFilter("")}>All</button>
             <button className="topics-button btn btn-primary" onClick={() => handleSetFilter("comedy")}>Comedy</button>
             <button className="topics-button btn btn-primary" onClick={() => handleSetFilter("video-essay")}>Video Essay</button>
             <button className="topics-button btn btn-primary" onClick={() => handleSetFilter("case-study")}>Start Up</button>
