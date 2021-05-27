764744e91b123fb6df79e1779c78e2bdd42b9fac
Md Shahazad Uddin <shanudjn@gmail.com>
2021-05-27 08:53:15 +0530

    page : added playlist page

--- src/pages/Playlist/playlist.jsx
+++ src/pages/Playlist/playlist.jsx
@@ -9,30 +9,40 @@ export function Playlist() {
 
     console.log(playlist)
     return (
-        <div className="container-playlist">
+        <div className="playlist-grid">
             {
                 playlist.map(({ _id, playlistName, videos }) => {
                     return (
                         <>
-                            <div className="div-title-playlist"><p className="title-playlist" >{playlistName}</p></div>
-                            {/* <div> */}
-                            {
-                                videos.map(({ id, title, url, thumbnail }) => {
+                            <div className="playlist-container">
+                                <div className="div-title-playlist"><span className="title-playlist" >{playlistName}</span><Link to="/" className="span-see-all">See All</Link></div>
+                                <div className="card-container">
+                                    {
+                                        videos.map(({ id, title, videoId, url, thumbnail }) => {
 
-                                    return (
-                                        <div className="card-horizontal" key={id}>
+                                            return (
+                                                <>
+                                                    <div className="playlist-card" key={id}>
 
-                                            <Link to={`/videopage/${id}`}><img src={thumbnail} alt="" className="playlist-video-thumbnail" /></Link>
+                                                        <Link to={`/videopage/${videoId}`}>
+                                                            <div>
+                                                                <img src={thumbnail} alt="" className="playlist-video-thumbnail" />
+                                                            </div>
+                                                        </Link>
+                                                        <div className="div-video-details">
+                                                            <Link to={`/videopage/${videoId}`}><span>{title}</span></Link>
+                                                        </div>
 
-                                            <div className="div-video-details">
-                                                <Link to={`/videopage/${id}`}><span>{title}</span></Link>
-                                            </div>
+                                                    </div>
+                                                </>
 
-                                        </div>
-                                    )
-                                })
-                            }
-                            {/* </div> */}
+
+                                            )
+                                        })
+                                    }</div>
+                            </div>
+
+
                         </>
 
                     )
