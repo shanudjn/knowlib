2634cc66224f2c7d7f8a7b36bd990663ea5c78e2
Md Shahazad Uddin <shanudjn@gmail.com>
2021-05-27 08:51:04 +0530

    Added mobile navbar

--- src/App.js
+++ src/App.js
@@ -9,12 +9,15 @@ import { Playlist } from './pages/Playlist/playlist';
 import { Video } from './pages/Video/video'
 import { Login } from './pages/Login/Login';
 import PrivateRoute from './PrivateRoute'
+import { Sidebar } from './components/Sidebar/Sidebar';
+import NavbarMobile from './components/Navbar-Mobile/NavbarMobile';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-
+      <Sidebar />
+      <NavbarMobile />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/saved" element={<Saved />} />
@@ -22,8 +25,6 @@ function App() {
         <Route path="/videopage/:videoId" element={<Video />} />
         <Route path="/login" element={<Login />} />
         <PrivateRoute path="/playlist" element={<Playlist />} />
-
-
       </Routes>
     </div>
   );
---
+++ src/components/Navbar-Mobile/NavbarMobile.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import './NavbarMobile.css';
+
+function NavbarMobile() {
+    return (
+        <div className="div-navbar-mobile">
+            <Link to='/' className='navbar-mobile-link'>
+                <div className="navbar-mobile-item">
+                    <span className="material-icons">
+                        home
+                     </span>
+                </div>
+                <span className='span-link-name'>Home</span>
+            </Link>
+            <Link to='/' className='navbar-mobile-link'>
+                <div className="navbar-mobile-item">
+                    <span className="material-icons">
+                        watch_later
+                    </span>
+                </div>
+                <span className='span-link-name'>Watch Later</span>
+            </Link>
+            <Link to='/playlist' className='navbar-mobile-link'>
+                <div className="navbar-mobile-item">
+                    <span className="material-icons">
+                        video_library
+                    </span>
+                </div>
+                <span className='span-link-name'>Library</span>
+            </Link>
+            <Link to='/' className='navbar-mobile-link'>
+                <div className="navbar-mobile-item">
+                    <span className="material-icons">
+                        account_circle
+                     </span>
+                </div>
+                <span className='span-link-name'>Login</span>
+            </Link>
+        </div>
+    )
+}
+
+export default NavbarMobile;
