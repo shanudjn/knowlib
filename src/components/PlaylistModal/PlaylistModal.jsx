c6c6ef97797271bce82ce31061ee69630f0556ff
Md Shahazad Uddin <shanudjn@gmail.com>
2021-05-27 08:54:17 +0530

    feat :  saving and deleting from playlist

--- src/components/PlaylistModal/PlaylistModal.jsx
+++ src/components/PlaylistModal/PlaylistModal.jsx
@@ -24,9 +24,9 @@ export function PlaylistModal({ showModal, videoId, handleShowModal }) {
 
         try {
             const createNewPlaylistResponse = await axios.post('http://localhost:8080/playlist/new', { playlistName: modalInput })
-            const addVideoToNewPlaylist = await axios.post('http://localhost:8080/playlist/add', { playlistName: modalInput, videoId: video.videoId })
+            const addVideoToNewPlaylist = await axios.post('http://localhost:8080/playlist/add', { playlistName: modalInput, videoId: video._id })
             if (createNewPlaylistResponse.status === 200 && addVideoToNewPlaylist.status === 200) {
-                console.log("Successfully Saved")
+                // console.log("Successfully Saved")
                 dispatch({ type: "ADD_PLAYLIST", payload: { playlistName: modalInput, video: video } })
                 setModalInput("");
             }
@@ -37,15 +37,20 @@ export function PlaylistModal({ showModal, videoId, handleShowModal }) {
     }
 
     function isChecked(playlistName, videoId) {
-
+        // console.log("Playlist Name", playlistName)
+        // console.log("VideoId", videoId)
         const videos = playlist.find((item) => item.playlistName === playlistName).videos
+        // console.log(videos)
 
         if (videos.length !== 0) {
             const video = videos.filter((item) => item.videoId === videoId)
-
-            if (video.length === 0) { return false; }
+            // console.log(video)
+            if (video.length !== 0) {
+                return true;
+            }
+            return false
         }
-        return true;
+        return false;
 
 
     }
@@ -55,30 +60,32 @@ export function PlaylistModal({ showModal, videoId, handleShowModal }) {
         console.log(videos)
         const foundVideo = videos.filter((item) => item.videoId === videoId)
         console.log(foundVideo)
-        if (foundVideo.length === 0) {
-            return false
+        if (foundVideo.length !== 0) {
+            console.log("video is present")
+            return true
         }
-        return true;
+        return false;
     }
 
     async function addToPlaylist(video, playlistName) {
         try {
-            const addToPlaylistResponse = await axios.post('https://video-lib-backend.herokuapp.com/playlist/add', { playlistName: playlistName, videoId: video.videoId })
-            console.log(addToPlaylistResponse)
+            const addToPlaylistResponse = await axios.post('http://localhost:8080/playlist/add', { playlistName: playlistName, videoId: video._id })
+            // console.log(addToPlaylistResponse)
             if (addToPlaylistResponse.status === 200) {
-                console.log("add to playlist")
+                // console.log("add to playlist")
                 dispatch({ type: "ADD_TO_PLAYLIST", payload: { video: video, playlistName: playlistName } })
             }
         } catch (error) {
             console.log(error)
         }
-        // dispatch({ type: "ADD_TO_PLAYLIST", payload: { video: video, playlistName: playlistName } })
+
     }
 
     async function removeFromPlaylist(video, playlistName) {
+        console.log("remove from playlist triggered")
         console.log(video.videoId, playlistName)
         try {
-            const removeFromPlaylistResponse = await axios.post('https://video-lib-backend.herokuapp.com/playlist/remove', { playlistName: playlistName, videoId: video.videoId })
+            const removeFromPlaylistResponse = await axios.post('http://localhost:8080/playlist/remove', { playlistName: playlistName, videoId: video._id })
             if (removeFromPlaylistResponse.status === 200) {
                 console.log("removed from playlist")
                 dispatch({ type: "REMOVE_FROM_PLAYLIST", payload: { video: video, playlistName: playlistName } })
@@ -92,16 +99,16 @@ export function PlaylistModal({ showModal, videoId, handleShowModal }) {
     function handleCheckbox(playlistName) {
 
         const video = videoList.find((item) => item.videoId === videoId)
-        console.log(video)
+        // console.log(video)
         const isVideoInPlaylist = checkInPlaylist(playlistName, videoId);
         console.log(isVideoInPlaylist)
-        !isVideoInPlaylist ? addToPlaylist(video, playlistName)
-            : removeFromPlaylist(video, playlistName)
+        if (isVideoInPlaylist === true) { removeFromPlaylist(video, playlistName) }
+        else {
+            addToPlaylist(video, playlistName)
+        }
     }
 
 
-
-    console.log(playlist)
     return (
         { showModal } && <div className={showModal ? "modal" : "modal hide"}>
             <div className="playlist-heading"><span>Playlists</span><span className="material-icons button-modal-close" onClick={handleShowModal}>
@@ -111,7 +118,7 @@ export function PlaylistModal({ showModal, videoId, handleShowModal }) {
 
             {
                 playlist.map((playlistItem, index) => {
-
+                    console.log(playlistItem)
                     return (
                         <>
 
